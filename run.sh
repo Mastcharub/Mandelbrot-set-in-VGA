16a98dd09312df86fd038b62543b693d80ae79e2
A. S <91565083+Mastcharub@users.noreply.github.com>
2025-04-19 02:48:29 +0200

    organized

--- run.sh
+++ run.sh
@@ -2,10 +2,10 @@ export PATH=$PATH:/usr/local/i386elfgcc/bin
 
 mkdir Binaries
 
-nasm "boot/boot.asm" -f bin -o "Binaries/boot.bin"
-nasm "boot/entry.asm" -f elf -o "Binaries/kernel_entry.o"
-i386-elf-gcc -ffreestanding -m32 -g -c "mandelbrot.c" -o "Binaries/kernel.o"
-nasm "boot/zero.asm" -f bin -o "Binaries/zeroes.bin"
+nasm "src/boot/boot.asm" -f bin -o "Binaries/boot.bin"
+nasm "src/boot/entry.asm" -f elf -o "Binaries/kernel_entry.o"
+i386-elf-gcc -ffreestanding -m32 -g -c "src/mandelbrot.c" -o "Binaries/kernel.o"
+nasm "src/boot/zero.asm" -f bin -o "Binaries/zeroes.bin"
 
 i386-elf-ld -o "Binaries/full_kernel.bin" -Ttext 0x1000 "Binaries/kernel_entry.o" "Binaries/kernel.o" --oformat binary
 
